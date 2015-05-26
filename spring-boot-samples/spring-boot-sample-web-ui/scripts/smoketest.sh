7257d42171305d3f649799d61051e181347634be
Andreas Folkesson <andreas.folkesson@gmail.com>
2015-05-26 09:19:58 +0200

    Script fix

--- spring-boot-samples/spring-boot-sample-web-ui/scripts/smoketest.sh
+++ spring-boot-samples/spring-boot-sample-web-ui/scripts/smoketest.sh
@@ -6,9 +6,9 @@ PORT=8080
 # Get the IPAdress of the docker instans
 DOODLE_IP=`docker inspect doodleshop-${INSTANCE} | grep IPAddress | awk -F'\"' '{print $4}'`
 # Basic smoketest, TODO: should be changed to environment?
-if ! `curl -f http://${DOODLE_IP}:${PORT}/doodles?author=Homer | grep 'Messages : View all' >/dev/null`
+if ! `curl -f http://${DOODLE_IP}:${PORT} | grep 'Messages : View all' >/dev/null`
 then
-    echo "Test failed (curl http://${DOODLE_IP}:${PORT}/doodles?author=Homer | grep 'Messages : View all' >/dev/null)"
+    echo "Test failed (curl http://${DOODLE_IP}:${PORT} | grep 'Messages : View all' >/dev/null)"
     exit 1
 fi
 # More tests here!
